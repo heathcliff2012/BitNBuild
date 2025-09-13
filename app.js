362450c8b473373f372836804032b820230269eb
Shaijo Johnson <shaijo0320@gmail.com>
2025-09-14 00:47:04 +0530

    updated

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 const app = express();
 const path = require('path');
 const userRoutes = require('./routes/user.routes');
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,34 @@
 const express = require('express');
+
+const { body } = require('express-validator');
 const router = express.Router();
+const multer = require('multer');
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/');
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + '-' + file.originalname);
+//   },
+// });
+const fileModel = require('../models/file.model');
+const upload = multer({ dest: 'uploads/' });
 
 router.get('/dashboard-admin', (req, res) => {
   res.render('dashboard-admin');
 });
+
+router.post('/upload-file', upload.single('file'), (req, res) => {
+  console.log(req.body);
+  console.log("File",req.file);
+  
+    // const { filename, mimetype, size, path } = req.file;
+    // const newFile = new fileModel({ filename, mimetype, size, path });
+    // newFile.save();
+    // alert('File uploaded successfully');
+    res.redirect('/transport');
+    
+  });
 
 router.get('/dashboard-public', (req, res) => {
   const username = req.cookies.username
@@ -18,8 +43,8 @@ router.get('/community-feedback', (req, res) => {
   res.render('feedback');
 });
 
-router.get('/ledger_transport.html', (req, res) => {
-  res.render('ledger_transport');
+router.get('/transport', (req, res) => {
+  res.render('transport');
 });
 
 module.exports = router;
